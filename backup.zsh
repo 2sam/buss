e575b48d6f019fcbf7150e73bbbff8d1fd8d3dc2
johannes <johannes@phantasie.cc>
2016-12-02 15:21:15 +0100

    structure formatting, add log function

--- backup.zsh
+++ backup.zsh
@@ -2,31 +2,46 @@
 
 if [[ ! $(id -u -n) == root ]]; then echo "run as root"; exit 1; fi
 
+#
+# VARS
+#
+
 DATE=$(date +"%Y%m%d%H%M")
 BACKUP_DIR=$(pwd)/$DATE
 if [[ -d "$1" ]]; then BACKUP_DIR=$1/$DATE; fi
 LOGFILE=$BACKUP_DIR/backup.log
 
 
+#
+# FUNCTIONS
+#
+
+putlog(){
+  echo "$1" >> $LOGFILE
+}
 
 # bu_dir DIR EXCLUDE_DIR1 EXCLUDE_DIR2...
 bu_dir(){
   excludes=()
-  for d in "${@:2}"; do 
-    excludes+=(--exclude "$d")
-  done
-
-  echo "${excludes[@]}"
-
-  echo -n "backing up $1 ... "
-  echo "backing up $1 ..." >> $LOGFILE
+  for d in "${@:2}"; do excludes+=(--exclude "$d"); done
   
+  echo -n "backing up $1 ... "
+  putlog "backing up $1 ..."
+
   # remove leading / if necessary
   tar cpjf $(echo $BACKUP_DIR/$(echo $1 | sed "s/\//_/g").tar.bz2 | sed "s/\/\//\//g") "${excludes[@]}" $1 &>> $LOGFILE
   echo "done"
 }
 
+
+#
+# RUNTIME
+#
+
+# PREREQUISITES
 mkdir -p $BACKUP_DIR
 touch $LOGFILE
 
-bu_dir /etc /etc/systemd /etc/ssh
+# BACKUP COMMANDS
+bu_dir /etc /etc/systemd /etc/sshi
+#bu_dir 
